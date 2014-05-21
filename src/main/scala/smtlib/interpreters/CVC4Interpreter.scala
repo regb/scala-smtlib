d4622f38a04a191798eb29f39d3c8b2ec312e811
Etienne Kneuss <colder@php.net>
2014-05-21 20:53:29 +0200

    Add CVC4 interpreter, fix incompatible "compatibility" option

---
+++ src/main/scala/smtlib/interpreters/CVC4Interpreter.scala
@@ -0,0 +1,39 @@
+package smtlib
+package interpreters
+
+import Commands._
+import CommandResponses._
+
+//import scala.sys.process._
+import java.io._
+
+class CVC4Interpreter extends Interpreter {
+
+  private val cvc4 = new ProcessBuilder("cvc4",
+                                                "-q",
+                                                "--produce-models",
+                                                "--incremental",
+                                                "--print-success",
+                                                "--lang", "smt").redirectErrorStream(true).start
+
+  val cvc4In = new BufferedWriter(new OutputStreamWriter(cvc4.getOutputStream))
+  val cvc4Out = new BufferedReader(new InputStreamReader(cvc4.getInputStream))
+
+  val parser = new ResponseParser(cvc4Out)
+
+  override def eval(cmd: Command): CommandResponse = {
+    PrettyPrinter(cmd, cvc4In)
+    cvc4In.write("\n")
+    cvc4In.flush
+    parser.next
+  }
+
+  override def free(): Unit = {
+    PrettyPrinter(Exit, cvc4In)
+    cvc4In.write("\n")
+    cvc4In.flush
+
+    cvc4.destroy
+    cvc4In.close
+  }
+}
--- src/main/scala/smtlib/sexpr/PrettyPrinter.scala
+++ src/main/scala/smtlib/sexpr/PrettyPrinter.scala
@@ -18,25 +18,20 @@ import java.io.StringWriter
 
 object PrettyPrinter {
 
-  def apply(sexpr: SExpr, writer: Writer, smtLibCompatibility: Boolean = false): Unit = sexpr match {
-    case SList(sexprs) => ppNary(writer, sexprs, "(", " ", ")", smtLibCompatibility)
+  def apply(sexpr: SExpr, writer: Writer): Unit = sexpr match {
+    case SList(sexprs) => ppNary(writer, sexprs, "(", " ", ")")
     case SString(s) => {
       writer.append('"')
       writer.write(s)
       writer.append('"')
     }
-    case SSymbol(s) if(smtLibCompatibility) => {
-      writer.write(s)
-    }
     case SSymbol(s) => {
-      writer.write("|")
       writer.write(s)
-      writer.write("|")
     }
     case SQualifiedSymbol(os, s) => {
-      os.foreach(apply(_, writer, smtLibCompatibility))
+      os.foreach(apply(_, writer))
       writer.append(':')
-      apply(s, writer, smtLibCompatibility)
+      apply(s, writer)
     }
     case SInt(i) => writer.write(i.toString)
     case SDouble(d) => writer.write(d.toString)
@@ -53,13 +48,13 @@ object PrettyPrinter {
     sWriter.toString
   }
 
-  private def ppNary(writer: Writer, exprs: Seq[SExpr], pre: String, op: String, post: String, smtLibCompatibility: Boolean = false): Unit = {
+  private def ppNary(writer: Writer, exprs: Seq[SExpr], pre: String, op: String, post: String): Unit = {
     writer.write(pre)
     var c = 0
     var sz = exprs.size
 
     exprs.foreach(e => {
-      apply(e, writer, smtLibCompatibility)
+      apply(e, writer)
       c += 1
       if(c < sz) writer.write(op)
     })
