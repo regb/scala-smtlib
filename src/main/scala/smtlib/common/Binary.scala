5a56300d9d2b6da16ca1f42ef06976ca76f7c697
Régis Blanc <regwblanc@gmail.com>
2015-02-21 00:43:21 +0100

    some basic conversion on Binary helper class

--- src/main/scala/smtlib/common/Binary.scala
+++ src/main/scala/smtlib/common/Binary.scala
@@ -1,11 +1,16 @@
-package com.on
+package smtlib
+package common
 
 case class Binary(repr: List[Boolean]) {
 
   //take the 32 least significant bits
-  def toBits: Int = {
-    val allReversedBits: List[Boolean] = repr.take(32).reverse.padTo(32, false)
-    allReversedBits.foldLeft(0)((bits, bit) => (bits | (if(bit) 1 else 0)) << 2)
+  def toIntBits: Int = {
+    ((toLongBits << 32) >>> 32).toInt
+  }
+
+  def toLongBits: Long = {
+    val allReversedBits: List[Boolean] = repr.take(64).reverse.padTo(64, false)
+    allReversedBits.foldRight(0l)((bit, bits) => ((bits<<1) | (if(bit) 1 else 0)))
   }
 
   //transform to a 32 bits integer, respecting 2 complements
---
+++ src/test/scala/smtlib/common/BinaryTests.scala
@@ -0,0 +1,38 @@
+package smtlib
+package common
+
+import org.scalatest.FunSuite
+
+class BinaryTests extends FunSuite {
+
+  test("toIntBits works with one bit") {
+    assert(Binary(List(true)).toIntBits === 1)
+    assert(Binary(List(false)).toIntBits === 0)
+  }
+
+  test("toIntBits works with a few bits") {
+    assert(Binary(List(true, true)).toIntBits === 3)
+    assert(Binary(List(true, false, true)).toIntBits === 5)
+  }
+
+  test("toIntBits correctly ignores leading zeros") {
+    assert(Binary(List(false, true)).toIntBits === 1)
+    assert(Binary(List(false, true, false, true)).toIntBits === 5)
+  }
+
+  test("toLongBits works with one bit") {
+    assert(Binary(List(true)).toLongBits === 1)
+    assert(Binary(List(false)).toLongBits === 0)
+  }
+
+  test("toLongBits works with a few bits") {
+    assert(Binary(List(true, true)).toLongBits === 3)
+    assert(Binary(List(true, false, true)).toLongBits === 5)
+  }
+
+  test("toLongBits correctly ignores leading zeros") {
+    assert(Binary(List(false, true)).toLongBits === 1)
+    assert(Binary(List(false, true, false, true)).toLongBits === 5)
+  }
+
+}
