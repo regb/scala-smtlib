de449956b60a688d41464a2cda397a00572a2e2b
Regis Blanc <regwblanc@gmail.com>
2015-02-23 12:41:43 +0100

    hide the exact implementation of binary

--- src/main/scala/smtlib/common/Binary.scala
+++ src/main/scala/smtlib/common/Binary.scala
@@ -1,7 +1,7 @@
 package smtlib
 package common
 
-case class Binary(repr: List[Boolean]) {
+class Binary private(val digits: List[Boolean]) {
 
   //take the 32 least significant bits
   def toIntBits: Int = {
@@ -9,14 +9,22 @@ case class Binary(repr: List[Boolean]) {
   }
 
   def toLongBits: Long = {
-    val allReversedBits: List[Boolean] = repr.take(64).reverse.padTo(64, false)
+    val allReversedBits: List[Boolean] = digits.take(64).reverse.padTo(64, false)
     allReversedBits.foldRight(0l)((bit, bits) => ((bits<<1) | (if(bit) 1 else 0)))
   }
 
   //transform to a 32 bits integer, respecting 2 complements
   def toInt: Int = {
-    val allReversedBits: List[Boolean] = repr.take(32).reverse.padTo(32, repr.head)
+    val allReversedBits: List[Boolean] = digits.take(32).reverse.padTo(32, digits.head)
     allReversedBits.foldRight(0)((bit, bits) => ((bits<<1) | (if(bit) 1 else 0)))
   }
+
+}
+
+object Binary {
+
+  def apply(digits: List[Boolean]) = new Binary(digits)
+
+  def apply(hexa: Hexadecimal) = new Binary(hexa.toBinary)
 
 }
