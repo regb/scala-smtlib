260fdaa9e81f76a057d78b5e1a8a378a50dfc21b
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-05-22 18:16:04 +0200

    CVC4 now prints 3 success per function in define-funs-rec

--- src/main/scala/smtlib/interpreters/CVC4Interpreter.scala
+++ src/main/scala/smtlib/interpreters/CVC4Interpreter.scala
@@ -14,9 +14,9 @@ class CVC4Interpreter(executable: String, args: Array[String]) extends ProcessIn
 
   override def parseResponseOf(cmd: Command): CommandResponse = cmd match {
     case DefineFunsRec(funs, bodies) =>
-      // CVC4 translates definefunsrec in two commands per function,
-      // and thus emits 2x (success)
-      val res = for (i <- 1 to funs.size*2) yield {
+      // CVC4 translates definefunsrec in three commands per function,
+      // and thus emits 3x (success)
+      val res = for (i <- 1 to funs.size*3) yield {
         parser.parseGenResponse
       }
 
