d2c23664c027e3f081c699239944eafb1da36611
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2020-11-25 01:15:00 +0100

    Do not rely on automatic activation

--- src/main/scala/smtlib/parser/ParserCommon.scala
+++ src/main/scala/smtlib/parser/ParserCommon.scala
@@ -70,7 +70,7 @@ trait ParserCommon {
    * Make sure the next token has the expected kind and read it
    */
   protected def eat(expected: TokenKind): Token = {
-    val token = nextToken
+    val token = nextToken()
     check(token, expected)
     token
   }
@@ -78,7 +78,7 @@ trait ParserCommon {
    * Make sure the next token is exactly ``expected`` (usually a symbol lit)
    */
   protected def eat(expected: Token): Token = {
-    val token = nextToken
+    val token = nextToken()
     if(token != expected)
       throw new UnexpectedTokenException(token, Seq(expected.kind))
     token
@@ -181,7 +181,7 @@ trait ParserCommon {
 
 
   def parseKeyword: SKeyword = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.Keyword(k) => {
         val keyword = SKeyword(k)
         keyword.setPos(t)
@@ -253,7 +253,7 @@ trait ParserCommon {
   }
 
   def parseSymbol: SSymbol = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.SymbolLit(s) => {
         val symbol = SSymbol(s)
         symbol.setPos(t)
@@ -270,7 +270,7 @@ trait ParserCommon {
    */
 
   def parseString: SString = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.StringLit(s) => {
         val str = SString(s)
         str.setPos(t)
@@ -280,7 +280,7 @@ trait ParserCommon {
   }
 
   def parseNumeral: SNumeral = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.NumeralLit(n) => {
         val num = SNumeral(n)
         num.setPos(t)
@@ -290,7 +290,7 @@ trait ParserCommon {
   }
 
   def parseDecimal: SDecimal = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.DecimalLit(n) => {
         val dec = SDecimal(n)
         dec.setPos(t)
@@ -300,7 +300,7 @@ trait ParserCommon {
   }
 
   def parseHexadecimal: SHexadecimal = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.HexadecimalLit(h) => {
         val hexa = SHexadecimal(h)
         hexa.setPos(t)
@@ -310,7 +310,7 @@ trait ParserCommon {
   }
 
   def parseBinary: SBinary = {
-    nextToken match {
+    nextToken() match {
       case t@Tokens.BinaryLit(b) => {
         val bin = SBinary(b.toList)
         bin.setPos(t)
